77a747c33392efa1901fa3445b14f7f4a52cfd15
Xingkai Yu <38156925+GeekEmperor@users.noreply.github.com>
2019-06-24 20:54:24 +0800

    Add files via upload

---
+++ lab/data-lab/bits.c
@@ -0,0 +1,387 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the BDD checker to formally verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 9.0.0.  Version 9.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, fourth edition, plus
+   Amd. 1  and Amd. 2 and 273 characters from forthcoming  10646, fifth edition.
+   (Amd. 2 was published 2016-05-01,
+   see https://www.iso.org/obp/ui/#iso:std:iso-iec:10646:ed-4:v1:amd:2:v1:en) */
+/* We do not support C11 <threads.h>.  */
+/*
+ * bitCount - returns count of number of 1's in word
+ *   Examples: bitCount(5) = 2, bitCount(7) = 3
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 40
+ *   Rating: 4
+ */
+int bitCount(int x) {
+/* add 32 bits by select appropriate netmask */
+	int x1=(0x55<<8)|0x55;
+	int y1=(x1<<16)|x1;
+	int x2=(0x33<<8)|0x33;
+	int y2=(x2<<16)|x2;
+	int x3=(0xF<<8)|0xF;
+	int y3=(x3<<16)|x3;
+	int y4=(0xFF<<16)|0xFF;
+	int y5=(0xFF<<8)|0xFF;
+	int a=(x&y1)+((x>>1)&y1);
+	int b=(a&y2)+((a>>2)&y2);
+	int c=(b&y3)+((b>>4)&y3);
+	int d=(c&y4)+((c>>8)&y4);
+	int e=(d&y5)+((d>>16)&y5);
+	return e;
+}
+/* 
+ * leastBitPos - return a mask that marks the position of the
+ *               least significant 1 bit. If x == 0, return 0
+ *   Example: leastBitPos(96) = 0x20
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2 
+ */
+int leastBitPos(int x) {
+/* x and its two's complement have the same leastBitPos */
+   return (~x+1)&x;
+}
+/* 
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateLeft(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 25
+ *   Rating: 3 
+ */
+int rotateLeft(int x, int n) {
+/* the n high bits shfit right,the 32-n low bits shfit left */
+	int a=x>>(33+~n);
+	int b=(0x1<<n)+~0x1+1;
+	int c=x<<n;
+	return (a&b)|c;
+}
+/* 
+ * isNotEqual - return 0 if x == y, and 1 otherwise 
+ *   Examples: isNotEqual(5,5) = 0, isNotEqual(4,5) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int isNotEqual(int x, int y) {
+/* if x==y,then x^y==0,else x^y!=0 */
+	return !!(x^y);
+}
+/* 
+ * sm2tc - Convert from sign-magnitude to two's complement
+ *   where the MSB is the sign bit
+ *   Example: sm2tc(0x80000005) = -5.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 4
+ */
+int sm2tc(int x) {
+/* if x==0,then c=0x0,else c=0xFFFFFFFF
+   if x>0,then a=0,result=x
+   if x<0,then b=0,result=x's two's complement
+*/
+	int y=x>>31;
+	int z=!!(x<<1);
+	int a=y&((~x+1)|(0x1<<31));
+	int b=~y&x;
+	int c=~z+1;
+	return c&(a|b);
+}
+/* 
+ * float_i2f - Return bit-level equivalent of expression (float) x
+ *   Result is returned as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point values.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_i2f(int x) {
+/* divide x into sign,fraction and exponent
+   shift left till the second highest bit is 1 and calculate the exponent
+   round according GRS
+   if carry happens,then exponent adds 1 and fraction shifts right
+*/
+	int sign=(x>>31)&0x1;
+	int fraction=0;
+	int exponent=157;
+	unsigned result=0;
+	int y=0;
+	fraction=sign?-x:x;
+	while(exponent>126&&!(fraction>>30))
+	{
+		fraction<<=1;
+		exponent--;
+	}
+	if(exponent<127)
+		exponent=0;
+	y=fraction&0x70;
+	if(y>0x40||(y==0x40&&((fraction&0x80)==0x80||fraction&0xF)))
+		fraction+=0x80;
+	if(fraction>>31)
+	{
+		fraction>>=1;
+		exponent++;
+	}
+	fraction=(fraction>>7)&0x7FFFFF;	
+	result=(sign<<31)|(exponent<<23)|fraction;
+	return result;
+}
+	
+/* 
+ * tmin - return minimum two's complement integer 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 4
+ *   Rating: 1
+ */
+int tmin(void) {
+/* too easy */
+	return 0x1<<31;
+}
+/* 
+ * isEqual - return 1 if x == y, and 0 otherwise 
+ *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int isEqual(int x, int y) {
+/* if x==y,then x^y==0,else x^y!=0 */
+	return !(x^y);
+}
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+/* also easy */
+	return ~(x&y)&~(~x&~y);
+}
+/* 
+ * isLessOrEqual - if x <= y  then return 1, else return 0 
+ *   Example: isLessOrEqual(4,5) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isLessOrEqual(int x, int y) {
+/* if x<0 and y>0,then a=0xFFFFFFFF,else a=0x0
+   if x>0 and y<0,then b=0xFFFFFFFF,else b=0x0
+   if y-x>0,then c=0xFFFFFFFF,else c=0x0
+*/
+	int a=(x>>31)&~(y>>31);
+	int b=~(x>>31)&(y>>31);
+	int c=~((y+~x+1)>>31);
+	return ~b&0x1&(a|c);
+}
+/* 
+ * bitMask - Generate a mask consisting of all 1's 
+ *   lowbit and highbit
+ *   Examples: bitMask(5,3) = 0x38
+ *   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
+ *   If lowbit > highbit, then mask should be all 0's
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int bitMask(int highbit, int lowbit) {
+/* 2^(highbit+1)-2^lowbit */
+	int a=~((highbit+~lowbit+1)>>31);
+	int b=(0x1<<highbit<<1)+~(0x1<<lowbit)+1;
+	return a&b;
+}
+/* 
+ * float_half - Return bit-level equivalent of expression 0.5*f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representation of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_half(unsigned uf) {
+/* if uf is infinity,then result=infinity
+   if uf is normalized,then exponent--
+   if uf's exponent is 1,it should be paid attention
+   if uf is unnormalized,then fraction/2
+*/
+	int a=uf>>31<<31;
+	int b=(uf>>23)&0xFF;
+	int c=uf&0x7FFFFF;
+	unsigned result=0;
+	if(b==0xFF)
+		result=uf;
+	else if(b>0x1)
+		result=a|((b-1)<<23)|c;
+	else if(b==0x1)
+		result=a|(((c|0x800000)>>1)+(c&(c>>1)&0x1));
+	else
+		result=a|((c>>1)+(c&(c>>1)&0x1));
+	return result;
+}
---
+++ lab/data-lab/decl.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 9.0.0.  Version 9.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, fourth edition, plus
+   Amd. 1  and Amd. 2 and 273 characters from forthcoming  10646, fifth edition.
+   (Amd. 2 was published 2016-05-01,
+   see https://www.iso.org/obp/ui/#iso:std:iso-iec:10646:ed-4:v1:amd:2:v1:en) */
+/* We do not support C11 <threads.h>.  */
+ {"bitCount", (funct_t) bitCount, (funct_t) test_bitCount, 1, "! ~ & ^ | + << >>", 40, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"leastBitPos", (funct_t) leastBitPos, (funct_t) test_leastBitPos, 1, "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"rotateLeft", (funct_t) rotateLeft, (funct_t) test_rotateLeft,
+   2, "! ~ & ^ | + << >>", 25, 3,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"isNotEqual", (funct_t) isNotEqual, (funct_t) test_isNotEqual, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"sm2tc", (funct_t) sm2tc, (funct_t) test_sm2tc, 1, "! ~ & ^ | + << >>", 15, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"tmin", (funct_t) tmin, (funct_t) test_tmin, 0, "! ~ & ^ | + << >>", 4, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isEqual", (funct_t) isEqual, (funct_t) test_isEqual, 2,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitXor", (funct_t) bitXor, (funct_t) test_bitXor, 2, "& ~", 14, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isLessOrEqual", (funct_t) isLessOrEqual, (funct_t) test_isLessOrEqual, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitMask", (funct_t) bitMask, (funct_t) test_bitMask, 2,
+    "! ~ & ^ | + << >>", 16, 3,
+  {{0, 31},{0,31},{TMin,TMax}}},
+ {"float_half", (funct_t) float_half, (funct_t) test_float_half, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
